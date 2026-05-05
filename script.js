88f04ad3fe4ec19ebcb9812e6a8b4cd2980737d9
Helium <130857426+Helium999@users.noreply.github.com>
2026-05-05 09:03:43 -0700

    Add an eventlistener to listen for keyboard inputs and fill in calculation variables accordingly

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function add(a, b) {
-    sum = a + b
+    const sum = a + b
     return sum
 }
 
 function subtract(a, b) {
-    difference = a - b
+    const difference = a - b
     return difference
 }
 
 function multiply(a, b) {
-    product = a*b
+    const product = a*b
     return product
 }
 
 function divide(a, b) {
-    quotient = a/b
+    const quotient = a/b
     return quotient
 }
 
@@ -22,3 +22,29 @@ function truncateDecimals(number, digits) {
     const multiplier = Math.pow(10, digits);
     return Math.trunc(number * multiplier) / multiplier;
 }
+
+let firstNum = ''
+let secondNum = ''
+let operator = ''
+
+document.addEventListener("keydown", event => {
+        const digits = ['1', '2', '3', '4', '5',
+                    '6', '7', '8', '9', '0']
+        const operators = ['+', '-', '*', '/']
+
+        if(digits.includes(event.key) && operator === '') {
+            firstNum = firstNum.concat(event.key)
+        }
+        else if(operators.includes(event.key) && firstNum !== '' && operator === '') {
+            operator = operator.concat(event.key)
+        }
+        else if(digits.includes(event.key) && operators != '') {
+            secondNum = secondNum.concat(event.key)
+        }
+        else if(event.key === 'Shift') {
+
+        }
+        else {
+            console.log("Invalid")
+        }
+})
